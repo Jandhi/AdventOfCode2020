2cb9308b6cf9193dffed8c36eaf42f88a778b16c
Lxcente <mlucente99@hotmail.com>
2022-01-25 20:43:06 -0500

    Day 2

--- day 2/problem.py
+++ day 2/problem.py
@@ -30,7 +30,30 @@ def count_valid_passwords(input) -> int:
         if is_valid(min_count, max_count, letter, password):
             count += 1
     return count
- 
+
+def is_valid_two(pos1, pos2, letter, password):
+    count = 0
+    pos1_has_letter = password[pos1 - 1] == letter
+    pos2_has_letter = password[pos2 - 1] == letter
+    if pos1_has_letter:
+        count += 1
+    if pos2_has_letter:
+        count += 1
+    if count == 1:
+        return True
+    else:
+        return False
+
+def count_valid_passwords_two(input) -> int:
+    count = 0 
+    for entry in input:
+        min_count, max_count, letter, password = entry
+        if is_valid_two(min_count, max_count, letter, password):
+            count += 1
+    return count
+
 print(
-    count_valid_passwords(input)
+    count_valid_passwords(input),
+    count_valid_passwords_two(input)
+
 )
